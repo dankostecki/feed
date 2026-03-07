2c7ce6b14ed65f09d4cf52462a47428b3649b79c
dankostecki <dancoder2025@gmail.com>
2026-03-07 23:06:42 +0100

    start

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/feed',
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
